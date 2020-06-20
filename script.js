e124bc3ac02c4107e837bab97e234ccfabe91b54
demi0504 <ripley.demi@gmail.com>
2020-06-20 00:26:16 -0500

    added ajax calls to cocktail api

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 //specific cocktail api
-//https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+const specificURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 //random cocktail api
 const randomURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 $(document).ready(function() {
     //event listener for random cocktail generator
-    //ajax call for random cocktail button
-    $.ajax({
-        url: randomURL,
-        type: "GET",
-        datatype: "jsonp",
-        success: function(data){
-            // console.log(data);
-            var cocktailName = data.drinks[0].strDrink
-            var cocktailMethod = data.drinks[0].strInstructions;
-            console.log(cocktailName, cocktailMethod);
+    $("#random-button").click(function(e){
+        event.preventDefault();
+        
+        //ajax call for random cocktail button
+        $.ajax({
+            url: randomURL,
+            type: "GET",
+            datatype: "jsonp",
+            success: function(data){
+                // console.log(data);
+                var cocktailName = data.drinks[0].strDrink;
+                var cocktailMethod = data.drinks[0].strInstructions;
+                console.log(cocktailName, cocktailMethod);
+                //populate cocktail recipe in div
+                $(".ran-recipe-name").html("<h5>Drink Name: " + cocktailName + "</h5>");
+                $(".ran-method").html("<h5>" + cocktailMethod + "</h5>");
             
-        }
+            }
+        });
     });
-    //populate cocktail recipe in div
+
+    //event listener for specific cocktail input
+    $("#search-recipe").click(function(e){
+        event.preventDefault();
+        // console.log("clicked ya")
+        var cocktail = $("#drink-input").val().trim();
+        //ajax call for specific recipe search
+        $.ajax({
+            url: specificURL + cocktail,
+            type: "GET",
+            datatype: "jsonp",
+            success: function(data){
+                console.log(data);
+                var cocktailName = data.drinks[0].strDrink;
+                var cocktailMethod = data.drinks[0].strInstructions;
+                $(".recipe-name").html("<h5>Drink Name: " + cocktailName + "</h5>");
+                $(".method").html("<h5>" + cocktailMethod + "</h5>");
+            }
+        })
+    })
 });
