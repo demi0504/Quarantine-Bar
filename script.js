5fc7327f79406660a01882e486882939f60c0a50
Billy Hicks <billy_jerome_jr@me.com>
2020-06-21 17:12:15 -0500

    the beginning of youtubes BS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+var channelName = 'ChilledCow'
+var vidWidth = 500;
+var vidHeight = 400;
+var vidResults = 10;
+
+
 //specific cocktail api
 const specificURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 //random cocktail api
 const randomURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+//youtube search api
+const specURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PL45D8CC8A81B1066E&key=AIzaSyBA6QxZm934iIkmS_3KtfzVjbPboIePXX4"
 $(document).ready(function() {
     //event listener for random cocktail generator
     $("#random-button").click(function(e){
@@ -44,6 +52,60 @@ $(document).ready(function() {
             }
         })
     })
+    
+    //event listener for youtube search
+    $(".search-button").click(function(e){
+        event.preventDefault();
+        
+    //     //ajax call for youtube search
+    //     $.ajax({
+    //         url: specURL,
+    //         type: "GET",
+    //         datatype: "jsonp",
+    //         success: function(data){
+    //             console.log(data)
+            
+    //         }
+    //     });
+    // });
+
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels",{
+            part: 'contentDetails',
+            forUsername: channelName,
+            key: 'AIzaSyBA6QxZm934iIkmS_3KtfzVjbPboIePXX4'},
+            function(data){
+                $.each(data.items, function(i, item){
+                //console.log(item);
+                    pid = item.contentDetails.relatedPlaylists.uploads;
+                    getVids(pid);
+                })
+            }
+    )}
+    )
+    
+    function getVids(pid){
+        $.get(
+            "https://www.googleapis.com/youtube/v3/playlistItems",{
+                part: 'snippet',
+                maxResults: vidResults,
+                playlistId: pid,
+            key: 'AIzaSyBA6QxZm934iIkmS_3KtfzVjbPboIePXX4'},
+                function(data){
+                    var output;
+                    $.each(data.items, function(i,item){
+                        console.log(item);
+                        videTitle = item.snippet.title;
+                        videoId = item.snippet.resourceId.videoId;
+
+                        output = '<li><iframe height="'+vidHeight+'" width="'+vidWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"</iframe></li>'
+
+                        $('.results').append(output);
+                    })
+                }
+        )
+    }
+
 });
 var currentTime = dayjs().format("H");
 
